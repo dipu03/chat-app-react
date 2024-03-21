474efa0570bcd82aa59398edb1d37eaefc0078b5
dipu03 <bhoumik.dipu@gmail.com>
2024-03-21 22:28:46 +0530

    changes for deployment

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -63,13 +63,14 @@ export const login = async (req, res) => {
 			return res.status(400).json({ error: "Invalid username or password" });
 		}
 
-		generateTokenAndSetCookie(user._id, res);
+		let jwt = generateTokenAndSetCookie(user._id, res);
 
 		res.status(200).json({
 			_id: user._id,
 			fullName: user.fullName,
 			username: user.username,
 			profilePic: user.profilePic,
+			jwt: jwt
 		});
 	} catch (error) {
 		console.log("Error in login controller", error.message);
