23e967bba50347499cd0460ae998c9e786f1cdcb
dipu03 <bhoumik.dipu@gmail.com>
2024-03-21 21:38:28 +0530

    changes for deployment

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,13 +1,18 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import cors from 'cors';
 
 const app = express();
+var corsOptions = {
+	origin: "https://chat-app-frontend-ny6x.onrender.com"
+  };
+app.use(cors(corsOptions));
 
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["https://chat-app-react-0zo5.onrender.com"],
+		origin: ["https://chat-app-frontend-ny6x.onrender.com"],
 		methods: ["GET", "POST"],
 	},
 });
