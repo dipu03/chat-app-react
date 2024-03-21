79ce64e628d97f895457a27d1e7765198227d368
dipu03 <bhoumik.dipu@gmail.com>
2024-03-22 00:21:00 +0530

    changes for deployment

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -26,7 +26,7 @@ const protectRoute = async (req, res, next) => {
 		next();
 	} catch (error) {
 		console.log("Error in protectRoute middleware: ", error.message);
-		res.status(500).json({ error: "Internal server error" });
+		res.status(500).json({ error: `${error.message} : Internal server error` });
 	}
 };
 
